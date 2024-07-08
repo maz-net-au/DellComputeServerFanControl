648ea21a93e7c0359800ca16f2321437eb0e069c
Maz <maz@maz.net.au>
2024-07-08 23:00:45 +1000

    Now interactive. failed attempt at controlling gpu performance states directly.

---
+++ ComputeServerTempMonitor/IPState.cs
@@ -0,0 +1,51 @@
+﻿//using NvAPIWrapper.Native.GPU;
+//using NvAPIWrapper.Native.Interfaces.GPU;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+//namespace ComputeServerTempMonitor
+//{
+//    public struct PState : IPerformanceStates20Info
+//    {
+//        IPerformanceStates20Info _available;
+//        PerformanceStateId _target;
+
+//        public PState(IPerformanceStates20Info available, PerformanceStateId target) 
+//        { 
+//            _available = available;
+//            _target = target;
+//        }
+//        public IReadOnlyDictionary<PerformanceStateId, IPerformanceStates20ClockEntry[]> Clocks
+//        {
+//            get
+//            {
+//                PerformanceStateId pid = _target;
+//                return _available.Clocks.Where(x => x.Key == pid).ToDictionary(x => x.Key, x => x.Value);
+//            }
+//        }
+//        public IPerformanceStates20VoltageEntry[] GeneralVoltages => _available.GeneralVoltages;
+
+//        public bool IsEditable => false;
+
+//        public IPerformanceState20[] PerformanceStates
+//        {
+//            get
+//            {
+//                PerformanceStateId pid = _target;
+//                return _available.PerformanceStates.Where(x => x.StateId == pid).ToArray();
+//            }
+//        }
+
+//        public IReadOnlyDictionary<PerformanceStateId, IPerformanceStates20VoltageEntry[]> Voltages
+//        {
+//            get
+//            {
+//                PerformanceStateId pid = _target;
+//                return _available.Voltages.Where(x => x.Key == pid).ToDictionary(x => x.Key, x => x.Value);
+//            }
+//        }
+//    }
+//}
--- ComputeServerTempMonitor/Program.cs
+++ ComputeServerTempMonitor/Program.cs
@@ -1,8 +1,16 @@
 ﻿using Newtonsoft.Json;
 using System;
 using System.Diagnostics;
+using System.Runtime.InteropServices.Marshalling;
 
 namespace ComputeServerTempMonitor;
+
+// TODO:
+// Output values on speed change
+// A key (c?) to reload config while live
+// A key (t?) to set the next cooldown (h:m?) to. e.g. i might set it to 100% for 8 hours while no one is here
+// idle detection (based on power usage? i still want to try and run things cooler)
+// 3d print a guide for air through the T4's
 
 class Program
 {
@@ -13,13 +21,19 @@ class Program
 
     static void Main(string[] args)
     {
+        string newMode = "x";
+        string mode = ""; // default to displaying temps
+
         CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
         bool IsRunning = true;
         int maxCPU = 0;
         int maxGPU = 0;
         int currentFanSpeed = 0;
 
-        Dictionary<string, string> currentStats = new Dictionary<string, string>();
+        //Dictionary<string, string> currentStats = new Dictionary<string, string>();
+        Dictionary<string, string> cpuTemps = new Dictionary<string, string>();
+        Dictionary<string, string> gpuTemps = new Dictionary<string, string>();
+        Dictionary<string, string> fanDetails = new Dictionary<string, string>();
 
         // read config for rules
         Config config;
@@ -80,7 +94,7 @@ class Program
                             if (gpuD.Length >= 3)
                             {
                                 int temp = int.Parse(gpuD[2]);
-                                currentStats[$"GPU_{gpuD[0]}"] = temp.ToString();
+                                gpuTemps[$"GPU_{gpuD[0]}"] = temp.ToString();
                                 newMaxGpu = Math.Max(newMaxGpu, temp);
                             }
                         }
@@ -121,7 +135,7 @@ class Program
                             if (cpuD[0] == "Temp")
                             {
                                 int temp = int.Parse(cpuD[4].Replace(" degrees C", ""));
-                                currentStats[$"CPU_{cpuNum}"] = temp.ToString();
+                                cpuTemps[$"CPU_{cpuNum}"] = temp.ToString();
                                 newMaxCpu = Math.Max(newMaxCpu, temp);
                                 cpuNum++;
                             }
@@ -139,59 +153,250 @@ class Program
             }
         }, cancellationTokenSource.Token);
         tCpu.Start();
-        int headerRow = 0;
+
+        // fan control thread
+        Task tFan = new Task(() =>
+        {
+            while (IsRunning)
+            {
+                try
+                {
+                    int newSpeed = 0;
+                    // watch for cpu cores + gpu cores.
+                    foreach (FanTempSpeeds fts in config.CPULimits)
+                    {
+                        if (maxCPU >= fts.Temp)
+                            newSpeed = Math.Max(newSpeed, fts.MinSpeed);
+                    }
+                    foreach (FanTempSpeeds fts in config.GPULimits)
+                    {
+                        if (maxGPU >= fts.Temp)
+                            newSpeed = Math.Max(newSpeed, fts.MinSpeed);
+                    }
+                    fanDetails["Fan_%"] = currentFanSpeed.ToString();
+                    fanDetails["To_%"] = newSpeed.ToString();
+                    if (newSpeed >= currentFanSpeed)
+                    {
+                        // once fans ramp, set a timer to delay the spin-down response until idle for a long time
+                        spinDown = DateTime.Now.AddSeconds(config.FanSpinDownDelay).Ticks;
+                    }
+                    if (newSpeed > currentFanSpeed || DateTime.Now.Ticks > spinDown)
+                    {
+                        // call IPMI commands to set fan speed
+                        result = exec(config.IPMIPath, $"-I {config.IPMIInterface}{config.IPMILogin} raw 0x30 0x30 0x02 0xff 0x{newSpeed.ToString("x2")}");
+                        currentFanSpeed = newSpeed;
+                    }
+                    fanDetails["Until"] = new DateTime(spinDown).ToString();
+                }
+                catch (Exception ex)
+                {
+                    Log("Error processing fan speeds: " + ex.ToString());
+                }
+            }
+        }, cancellationTokenSource.Token);
+        tFan.Start();
+
+        // put in our external control loop here
+        // it'll be a state-machine with the current mode
+
         while (IsRunning)
         {
             try
             {
-                int newSpeed = 0;
-                // watch for cpu cores + gpu cores.
-                foreach (FanTempSpeeds fts in config.CPULimits)
+                if (Console.KeyAvailable)
                 {
-                    if (maxCPU >= fts.Temp)
-                        newSpeed = Math.Max(newSpeed, fts.MinSpeed);
+                    ConsoleKeyInfo key = Console.ReadKey(true);
+                    newMode = key.KeyChar.ToString().ToLower();
+                    if (newMode == mode)
+                    {
+                        continue;
+                    }
+                    mode = newMode;
+                    /*
+                        "d" to dispay current state. press any key to stop
+                        "s" to set a new fan speed
+                        "r" to reset, and ignore the cooldown timer
+                        "f" for finished, it puts the GPUs into an idle state (P8 or P12 if possible), waits some time and then slows the fans
+                        "w" for wake, puts the GPUs into P0
+                    */
+                    switch (mode)
+                    {
+                        case "d":
+                            Log("Dispaying current state. Press any key to cancel.");
+                            int headerRow = 0;
+                            while (!Console.KeyAvailable)
+                            {
+                                string headers = "";
+                                string values = "";
+
+                                foreach (string s in cpuTemps.Keys)
+                                {
+                                    headers += $"{s}\t";
+                                    values += $"{cpuTemps[s]}\t";
+                                }
+                                foreach (string s in gpuTemps.Keys)
+                                {
+                                    headers += $"{s}\t";
+                                    values += $"{gpuTemps[s]}\t";
+                                }
+                                foreach (string s in fanDetails.Keys)
+                                {
+                                    headers += $"{s}\t";
+                                    values += $"{fanDetails[s]}\t";
+                                }
+                                if (headerRow == 0)
+                                {
+                                    Log(headers);
+                                }
+                                Log(values);
+                                headerRow = (headerRow + 1) % 20;
+                                Thread.Sleep(1000);
+                            }
+                            break;
+                        case "s":
+                            Log("Setting a new fan speed. Enter a number between 0 and 100");
+                            string rIn = Console.ReadLine() ?? "";
+                            int newSpeed = 0;
+                            if (int.TryParse(rIn, out newSpeed))
+                            {
+                                if (newSpeed < 0)
+                                    newSpeed = 0;
+                                if (newSpeed > 100)
+                                    newSpeed = 100;
+                                Log($"Setting fan speed to {newSpeed}%");
+                                result = exec(config.IPMIPath, $"-I {config.IPMIInterface}{config.IPMILogin} raw 0x30 0x30 0x02 0xff 0x{newSpeed.ToString("x2")}");
+                                currentFanSpeed = newSpeed;
+                                spinDown = DateTime.Now.AddSeconds(config.FanSpinDownDelay).Ticks;
+                            }
+                            else
+                            {
+                                Log($"Invalid speed entered: '{rIn}'");
+                            }
+                            break;
+                        case "r":
+                            Log("Resetting cooldown timer.");
+                            spinDown = DateTime.Now.Ticks;
+                            break;
+                        case "t":
+                            Log("Enter a new delay as h:mm or mmm.");
+                            string time = Console.ReadLine() ?? "";
+                            if (time.Contains(':'))
+                            {
+                                // hh:mm
+                                string[] parts = time.Split(':');
+                                if (parts.Length == 2)
+                                {
+                                    int hours = 0, minutes = 0;
+                                    if (int.TryParse(parts[0], out hours) && int.TryParse(parts[1], out minutes))
+                                    {
+                                        DateTime endDate = DateTime.Now.AddMinutes((hours * 60) + minutes);
+                                        spinDown = endDate.Ticks;
+                                        Log($"Fan speed timeout will expire at {endDate}");
+                                    }
+                                }
+                            }
+                            else
+                            {
+                                int minutes = 0;
+                                if (int.TryParse(time, out minutes))
+                                {
+                                    DateTime endDate = DateTime.Now.AddMinutes(minutes);
+                                    spinDown = endDate.Ticks;
+                                    Log($"Fan speed timeout will expire at {endDate}");
+                                }
+                            }
+                            break;
+                        case "c":
+                            if (File.Exists("config.json"))
+                            {
+                                config = JsonConvert.DeserializeObject<Config>(File.ReadAllText("config.json")) ?? new Config();
+                                Log("config.json loaded");
+                            }
+                            break;
+                        //case "f":
+                        //    Log("Putting all the GPUs into a low power state.");
+                        //    // P8? P12?
+                        //    try
+                        //    {
+                        //        PhysicalGPUHandle[] handles = GPUApi.EnumTCCPhysicalGPUs();
+                        //        foreach (PhysicalGPUHandle ph in handles)
+                        //        {
+                        //            IPerformanceStates20Info pi = GPUApi.GetPerformanceStates20(ph);
+                        //            var lowPowerState = new PState(pi, NvAPIWrapper.Native.GPU.PerformanceStateId.P8_HDVideoPlayback);
+                        //            Console.WriteLine(JsonConvert.SerializeObject(lowPowerState));
+                        //            NvAPIWrapper.GPU.PhysicalGPU[] gpus = NvAPIWrapper.GPU.PhysicalGPU.GetTCCPhysicalGPUs();
+                        //            foreach (NvAPIWrapper.GPU.PhysicalGPU gpu in gpus)
+                        //            {
+                        //                gpu.
+                        //            }
+                        //            GPUApi.SetPerformanceStates20(ph, pi);
+                        //        }
+                        //    }
+                        //    catch (Exception ex)
+                        //    {
+                        //        Log(ex.ToString());
+                        //    }
+                        //    break;
+                        //case "v":
+                        //    try
+                        //    {
+                        //        PhysicalGPUHandle[] handles = GPUApi.EnumTCCPhysicalGPUs();
+                        //        foreach (PhysicalGPUHandle ph in handles)
+                        //        {
+                        //            IPerformanceStates20Info pi = GPUApi.GetPerformanceStates20(ph);
+                        //            Console.WriteLine(JsonConvert.SerializeObject(pi));
+                        //        }
+                        //    }
+                        //    catch (Exception ex)
+                        //    {
+                        //        Log(ex.ToString());
+                        //    }
+                        //    break;
+                        //case "w":
+                        //    Log("Restoring all the GPU to a high power state.");
+                        //    try
+                        //    {
+                        //        PhysicalGPUHandle[] handles2 = GPUApi.EnumTCCPhysicalGPUs();
+                        //        foreach (PhysicalGPUHandle ph in handles2)
+                        //        {
+                        //            IPerformanceStates20Info pi = GPUApi.GetPerformanceStates20(ph);
+                        //            GPUApi.SetPerformanceStates20(ph, pi); // restore all states? or did i just overwrite these until I reboot?
+                        //        }
+                        //    }
+                        //    catch (Exception ex)
+                        //    {
+                        //        Log(ex.ToString());
+                        //    }
+                        //    break;
+                        case "x":
+                            IsRunning = false;
+                            // restore the fan states so that nothing catches fire
+                            exec(config.IPMIPath, $"-I {config.IPMIInterface}{config.IPMILogin} raw 0x30 0x30 0x01 0x01");
+                            Log("Disabled manual control.");
+                            exec(config.IPMIPath, $"-I {config.IPMIInterface}{config.IPMILogin} raw 0x30 0xce 0x00 0x16 0x05 0x00 0x00 0x00 0x05 0x00 0x01 0x00 0x00");
+                            Log("Restored PCIe cooling response.");
+                            break;
+                        default:
+                            Log("'d' to dispay current state. press any key to stop");
+                            Log("'s' to set a new fan speed.");
+                            Log("'r' to reset, and ignore the cooldown timer.");
+                            Log("'c' to reload the configuration file.");
+                            Log("'t' to set a new cooldown delay.");
+                            //Log("'f' for finished, it puts the GPUs into an idle state(P8 or P12 if possible), waits some time and then slows the fans.");
+                            //Log("'w' for wake, which restores the high power states.");
+                            Log("'x' to exit");
+                            break;
+                    }
                 }
-                foreach (FanTempSpeeds fts in config.GPULimits)
-                {
-                    if (maxGPU >= fts.Temp)
-                        newSpeed = Math.Max(newSpeed, fts.MinSpeed);
-                }
-                currentStats["Fan_%"] = currentFanSpeed.ToString();
-                currentStats["To_%"] = newSpeed.ToString();
-                if (newSpeed >= currentFanSpeed)
-                {
-                    // once fans ramp, set a timer to delay the spin-down response until idle for a long time
-                    spinDown = DateTime.Now.AddSeconds(config.FanSpinDownDelay).Ticks;
-                }
-                if (newSpeed > currentFanSpeed || DateTime.Now.Ticks > spinDown)
-                {
-                    // call IPMI commands to set fan speed
-                    result = exec(config.IPMIPath, $"-I {config.IPMIInterface}{config.IPMILogin} raw 0x30 0x30 0x02 0xff 0x{newSpeed.ToString("x2")}");
-                    currentFanSpeed = newSpeed;
-                }
-                currentStats["Until"] = new DateTime(spinDown).ToString();
-                string headers = "";
-                string values = "";
-                foreach (string s in new List<string>() { "CPU_0", "CPU_1", "GPU_0", "GPU_1", "Fan_%", "To_%", "Until"})
-                {
-                    headers += $"{s}\t";
-                    values += $"{(currentStats.ContainsKey(s) ? currentStats[s] : "-")}\t";
-                }
-                if (headerRow == 0)
-                {
-                    Log(headers);
-                }
-                Log(values);
-                headerRow = (headerRow + 1) % 20;
-                Thread.Sleep(1000);
             }
             catch (Exception ex)
             {
-                Log("Error setting fan speeds: " + ex.ToString());
+                Log("In input loop: " + ex.ToString());
             }
         }
         cancellationTokenSource.Cancel();
     }
+
 
     static List<string> exec(string command, string args)
     {
